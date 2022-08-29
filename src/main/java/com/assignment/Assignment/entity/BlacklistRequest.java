5204b045f641af0c05e9ab480f0440dc5aa7645e
shivamchandak <shivam.chandak@meesho.com>
2022-08-29 23:29:53 +0530

    removed redundant usage of primary and secondary in packages.

---
+++ src/main/java/com/assignment/Assignment/entity/BlacklistRequest.java
@@ -0,0 +1,17 @@
+package com.assignment.Assignment.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BlacklistRequest {
+    private List<String> phoneNumbers;
+}
